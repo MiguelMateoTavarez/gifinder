f938add97d2fdc6ccb726b10b34669cb533abf7f
Miguel Mateo <miguelmateot94@gmail.com>
2023-08-08 23:52:06 -0400

    Modify existing files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Gifinder } from './Gifinder.jsx'
+
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Gifinder />
   </React.StrictMode>,
 )
