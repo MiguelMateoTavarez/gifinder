1104c356ac178447bc4ea745d3b61aac6dd182f2
Miguel Mateo <miguelmateot94@gmail.com>
2023-08-09 15:51:03 -0400

    Gifinder completed

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,24 +1,17 @@
 import PropTypes from 'prop-types';
-import { getGifs } from '../helpers/getGifs';
-import { useEffect, useState } from 'react';
 import { GifGridItem } from './GifGridItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
 
-  const [images, setImages] = useState([]);
-
-  const getImages = async() => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  }
-
-  useEffect( () => {
-    getImages();
-  }, []);
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
+      {
+        isLoading && (<h2 className='card'>Loading...</h2>)
+      }
       <div className='card-grid'>
         {
           images.map( (image) => (
