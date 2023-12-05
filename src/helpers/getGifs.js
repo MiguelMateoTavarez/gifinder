bc9e2d241bf46c609867b431ef680160b003810c
Miguel Mateo <miguelmateot94@gmail.com>
2023-12-05 08:57:07 -0400

    Fix url

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@ const apiKey = import.meta.env.VITE_API_KEY;
 
 export const getGifs = async (category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}=${ category }&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${ category }&limit=10`;
     const resp = await fetch(url);
 
     const { data } = await resp.json();
